be0757458ad05df67307235f5fe0cc764152dff6
Paul Rojas <paulrojas@iMac-de-Paul.local>
2017-01-03 23:13:04 -0500

    Test

--- js/jquery.fileupload-angular.js
+++ js/jquery.fileupload-angular.js
@@ -118,7 +118,7 @@
                         if ((scope.option('autoUpload') ||
                                 data.autoUpload) &&
                                 data.autoUpload !== false) {
-                            if (data.form[0].action != undefined)
+                            if (data.form[0] != undefined)
                                 data.submit();
                         }
                     });
